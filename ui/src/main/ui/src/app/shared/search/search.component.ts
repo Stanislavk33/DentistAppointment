ecd6879c982482987576ea7c98983c1e060d7ba5
nikolaraevskiregs <nikolaraevskiregs@gmail.com>
2017-11-19 18:03:40 +0200

    Introduced basic routing mechanism; it's not finalized.

---
+++ ui/src/main/ui/src/app/shared/search/search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'search-component',
+  templateUrl: 'search.component.html',
+  styleUrls: ["search.component.css"],
+  providers: []
+})
+export class SearchComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
